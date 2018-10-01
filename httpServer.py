037d47ab18a359ab8088e120ba72c2075350e67e
Deepti Garg <deepti7garg@gmail.com>
2018-10-01 12:28:08 -0700

    updated code

--- httpServer.py
+++ httpServer.py
@@ -14,15 +14,12 @@ class CustomHandler(BaseHTTPRequestHandler):
       self.end_headers()
 
       # Send the html message
-      htmlMsg= "<html><head><title>%s output - %s</title></head><body><p>URL:</p></body>\
-	  </html>" 
-	 
-	 #"<h2>Hello World!</h2><button type='button'>Click me!</button>"
-	  
-	  
+      htmlMsg= "<html><head><title>Sample Web Server</title></head><body><p><h2>Hello World!</h2></p>\
+	  <button type='button'>Click me!</button></body>\
+	  </html>" 	  
 
       self.wfile.write(htmlMsg.encode()) #Python 3
-<button type="button">Click Me!</button>
+
       return
 
 if __name__ == '__main__':
